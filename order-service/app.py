107d3f6a8177b41cf26bef1e2197c253bdcfc955
Rahull Ganesh M <rahull.ganesh@idp.com>
2026-08-04 16:45:43 +0530

    Fix syntax error in order-service imports

--- order-service/app.py
+++ order-service/app.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.core import patch_all
 from aws_xray_sdk.ext.fastapi.middleware import XRayMiddleware
 
 patch_all()
 xray_recorder.configure(service='order-service')
-, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from mangum import Mangum
